4880318c09961bcd6cc82da4275df13d22eeb8a6
Mikhail Nikitin <nitinin.miha@icloud.com>
2023-06-18 21:31:47 +0300

    Made repo methods suspend

--- common-repo/src/commonMain/kotlin/mikhailsnnt/taxresident/common/repo/period/IPeriodRepository.kt
+++ common-repo/src/commonMain/kotlin/mikhailsnnt/taxresident/common/repo/period/IPeriodRepository.kt
@@ -3,18 +3,18 @@ package mikhailsnnt.taxresident.common.repo.period
 import mikhailsnnt.taxresident.common.model.wrappers.TxUserId
 
 interface IPeriodRepository {
-    fun create(periodRequest: PeriodRequest): PeriodDbResponse
-    fun read(userId: TxUserId): PeriodsDbResponse
-    fun update(periodRequest: PeriodRequest): PeriodDbResponse
-    fun delete(periodIdRequest: PeriodIdRequest): PeriodDbResponse
+    suspend fun create(periodRequest: PeriodRequest): PeriodDbResponse
+    suspend fun read(userId: TxUserId): PeriodsDbResponse
+    suspend fun update(periodRequest: PeriodRequest): PeriodDbResponse
+    suspend fun delete(periodIdRequest: PeriodIdRequest): PeriodDbResponse
 
     companion object {
         @Suppress("unused")
         val NONE = object : IPeriodRepository {
-            override fun create(periodRequest: PeriodRequest): PeriodDbResponse = throw NotImplementedError()
-            override fun read(userId: TxUserId): PeriodsDbResponse = throw NotImplementedError()
-            override fun update(periodRequest: PeriodRequest): PeriodDbResponse = throw NotImplementedError()
-            override fun delete(periodIdRequest: PeriodIdRequest): PeriodDbResponse = throw NotImplementedError()
+            override suspend fun create(periodRequest: PeriodRequest): PeriodDbResponse = throw NotImplementedError()
+            override suspend fun read(userId: TxUserId): PeriodsDbResponse = throw NotImplementedError()
+            override suspend fun update(periodRequest: PeriodRequest): PeriodDbResponse = throw NotImplementedError()
+            override suspend fun delete(periodIdRequest: PeriodIdRequest): PeriodDbResponse = throw NotImplementedError()
         }
     }
 }
